a7e2962fdcfea76e327d37832c25b8eb7c92e16b
Sharando <catlordd@gmail.com>
2024-02-06 10:36:26 +0300

    6.02

--- game.py
+++ game.py
@@ -3,7 +3,9 @@ from tkinter import *
 root = Tk()     # создаем корневой объект - окно
 root.title("2024")     # устанавливаем заголовок окна
 root.geometry("500x600")    # устанавливаем размеры окна
-root.resizable(False, False) 
+root.resizable(False, False)
+icon = PhotoImage(file = "data/icon.png")
+root.iconphoto(False, icon)
 label = Label(text="Hello, it is 2024!") # создаем текстовую метку
 label.pack()    # размещаем метку в окне
  
